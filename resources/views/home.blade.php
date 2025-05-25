9cf8fdaa33259cb4a35075816339d19ab72fd3d9
jessy <jessyhneino@gmial.com>
2025-05-25 13:29:09 +0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,246 @@
+@extends('layouts.master')
+
+@section('title')
+HOME
+@endsection
+
+@section('content') 
+
+        <!-- ------------------------------------------ section home-------------------------------------  -->
+
+
+        <div class="home " id="Home" >
+            <div class="HPbt">
+                <div class="HP">
+                    <h3 class="H">Lavender Ladies</h3>
+                    <p class="P">
+                        we offer you a variety of sports <br>
+                        fashion specially designed to meet the needs <br>of women who are seeking sport with elegance and comfort.<br> 
+                        We guarantee you high quality and distinctive <br> designs that suit all kinds of sports,
+                        which helps you achieve your sports goals with confidence.<br>
+                        Go towards a healthy and active lifestyle with our wonderful collection!
+                    </p>
+                </div>
+                <a class="level-nav-link    nav-link" href="#browse"><button class="btn1">Get Started</button>   </a>
+            </div>
+            <img src="{{asset('images/logostar3.png')}}" class="yoga">
+
+        </div>
+
+        <!-- ------------------------------------------ end section home-------------------------------------  -->
+
+
+        <!-- ------------------------------------------ section cards id-------------------------------------  -->
+
+        <div class="card-container">
+            <div class="card-home">
+                <img src="{{asset('images/photo2.jpg')}}">
+                <div class="card-content">
+                    <h3>Graphic Design</h3>
+                    <p>
+                        Graphic design for women combines elegance and creativity,
+                         where focusing on attractive colors and innovative 
+                         styles reflecting strength and femininity. 
+                         Features personalized designs, and unique optical elements 
+                         highlight beauty and excellence. The design embodies the vision
+                          of modern women and enhances its presence in the world of art and technology.
+                    </p>
+                   
+                </div>
+            </div>
+
+
+            <div class="card-home">
+                <img src="{{asset('images/photo1.jpg')}}">
+                <div class="card-content ">
+                    <h3  >Fashion Designer</h3>
+                    <p>
+                        Fashion designer is a creative artist that embodies elegance
+                        and innovation in every design.  It is characterized by a unique vision
+                        that combines colors, fabrics, and fine details, which creates pieces that reflect the p
+                        ersonality of the modern woman.
+                        Through its designs, it transmits the messages of strength and trust, making each piece
+                        of artistic masterpiece tells a unique story.
+                    </p>
+                  
+                </div>
+            </div>
+     
+            <div class="card-home">
+                <img src="{{asset('images/photo3.jpg')}}">
+                <div class="card-content">
+                    <h3>Photographer</h3>
+                    <p>
+                        The photographer is an artist who captures enchanting moments in a unique style,
+                         reflecting the spirit of femininity and the details of life, coloring them with
+                          magical hues. Her lens is used to tell inspiring stories, showcasing emotions
+                           and colors, making each image a masterpiece that expresses the beauty of the
+                            world from her own perspective.                   
+                    </p>
+                </div>
+            </div>
+
+            <div class="card-home">
+                <img src="{{asset('images/photo4.jpg')}}">
+                <div class="card-content">
+                    <h3>Casual Fashion</h3>
+                    <p>
+                        Casual costumes for women combine comfort and elegance, 
+                        as they feature simple designs and comfortable colors. 
+                         It includes pieces such as jeans, loose shirts, and sports shoes,
+                          giving women a modern and comfortable look that fits various occasions. 
+                           These costumes reflect
+                        the character of the modern woman who seeks to balance elegance and comfort.
+                    </p> 
+                </div>
+            </div>
+        </div>
+<!-- ------------------------------------------end section cards id-------------------------------------  -->
+
+<!-- ------------------------------------------section clock-------------------------------------  -->
+
+        <div class="clh">
+            <p>Set for yourself an hour a day,and enjoy the journey<br>
+                of the transformation towards a healthy and attractive body.<br>
+                with our comfortable and distinguished clothes of the club.
+            </p>
+            <div class="clock" id="time">
+                <div class="circle" style="--clr:#FF389C">  
+                    <div class="dots  hr_dot"></div>
+                    <svg>
+                        <circle  cx="70" cy="70" r="70"></circle>
+                        <circle  cx="70" cy="70" r="70" id="hh" ></circle>
+                    </svg>
+                    <div id="hours">00</div>
+                </div>
+                <div class="circle"   style="--clr:#fee800">
+                    <div class="dots  min_dot"></div>
+                    <svg>
+                        <circle  cx="70" cy="70" r="70"></circle>
+                        <circle  cx="70" cy="70" r="70"  id="mm"></circle>
+                    </svg>
+                    <div id="minutes"> 00</div>
+                </div>
+                <div class="circle"   style="--clr:#04fc43">
+                    <div class="dots  sec_dot"></div>
+                    <svg>
+                        <circle  cx="70" cy="70" r="70"></circle>
+                        <circle  cx="70" cy="70" r="70"  id="ss"></circle>
+                    </svg>
+                    <div id="seconds">00</div>
+                </div>
+                <div class="ap">
+                    <div id="ampm">
+                         AM
+                    </div>
+                </div>
+            </div>       
+        </div>
+
+<!-- ------------------------------------------end section clock-------------------------------------  -->
+
+
+<!-- ------------------------------------------ section swiper-------------------------------------  -->
+
+
+        <section class="cloth" id="browse">
+          
+                <div class="swiper  cloth-slider">
+                        <div class="swiper-wrapper">
+                                <div class="swiper-slide   slide">
+                                        <img src="{{asset('images/shose.jpg')}}" alt="" class="user-image">
+                                        <h2 class="user-name">Sneakers</h2>
+                                        <a href="#shose">
+                                            <button class="btn1 message-button" >Look at</button> 
+                                        </a>
+                                </div>
+                                <div class="swiper-slide   slide">
+                                        <img src="{{asset('images/Vison.jpg')}}" alt="" class="user-image">
+                                        <h2 class="user-name">Leggings</h2>
+                                        <a href="#browse">
+                                            <button class="btn1 message-button">Look at</button> 
+                                        </a>
+                                </div>
+                                <div class="swiper-slide   slide">
+                                        <img src="{{asset('images/sweater2.jpg')}}" alt="" class="user-image">
+                                        <h2 class="user-name">Sweaters</h2>
+                                        <a href="#browse">
+                                            <button class="btn1 message-button">Look at</button> 
+                                        </a>
+                                </div>
+                                <div class="swiper-slide   slide">
+                                        <img src="{{asset('images/jacket.jpg')}}" alt="" class="user-image">
+                                        <h2 class="user-name">Jackets</h2>
+                                        <a href="#browse">
+                                            <button class="btn1 message-button">Look at</button> 
+                                        </a>
+                                </div>
+                                <div class="swiper-slide   slide">
+                                        <img src="{{asset('images/sock.jpg')}}" alt="" class="user-image">
+                                        <h2 class="user-name">Socks</h2>
+                                                
+                                        <a href="#browse">
+                                            <button class="btn1 message-button">Look at</button> 
+                                        </a>
+                                </div>
+                                <div class="swiper-slide   slide">
+                                        <img src="{{asset('images/towel.jpg')}}" alt="" class="user-image">
+                                        <h2 class="user-name">Towels</h2>
+                                        <a href="#browse">
+                                            <button class="btn1 message-button">Look at</button> 
+                                        </a>
+                                </div>
+                                <div class="swiper-slide   slide">
+                                        <img src="{{asset('images/suit.jpg')}}" alt="" class="user-image">
+                                        <h2 class="user-name">Tracksuits</h2>
+                                        <a href="#browse">
+                                            <button class="btn1 message-button">Look at</button> 
+                                        </a>
+                                </div>
+                        </div>
+                        <div class="swiper-button-next"></div>
+                        <div class="swiper-button-prev"></div>
+                        <div class="swiper-pagination"></div>
+                    
+                </div>
+        </section>
+
+<!-- ------------------------------------------end section swiper-------------------------------------  -->
+
+
+<!-- ------------------------------------------section map-------------------------------------  -->
+
+
+        <div class="map">
+            <!-- <h1>Google Map!</h1> -->
+            <div class="mapd">
+                <iframe class="mapp" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d621714.
+                    0783297188!2d14.084112070547121!3d52.50501170704109!2m3!1f0!2f0!3f0!3m2!1i1024!2i76
+                    8!4f13.1!3m3!1m2!1s0x47a84e373f035901%3A0x42120465b5e3b70!2z2KjYsdmE2YrZhtiMINij2YT
+                    Zhdin2YbZitin!5e0!3m2!1sar!2s!4v1728457964551!5m2!1sar!2s">
+                </iframe>       
+                <div class="details">
+                    <h2>Visit one of our agency locations or contact us today </h2>
+                    <h3>Head Office</h3>
+                    <div class="inform">
+                        <p>  <i  class="fa-solid fa-flag"></i>    56 Glassford Street Glassgow GI IUL New York</p>
+                        <p>   <i  class="fa-solid fa-message"></i>    contact@lavender.com</p>
+                        <p>  <i  class="fa-solid fa-phone"></i>    contact@lavender.com</p>
+                        <p> <i  class="fa-solid fa-clock"></i>    Monday to Saturday :9.00am to 16pm</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<!-- ------------------------------------------end section map-------------------------------------  -->
+
+
+
+            
+        
+        <h1 class="text-3xl font-bold underline bg-sky-500 w-32 h-32">
+                 Hello world!
+        </h1>
+            
+
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    
+    
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+    <title> @yield('title')</title>
+    <link rel="stylesheet" href="{{ asset( 'css/style.css') }}">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <!-- <link rel="stylesheet" href="https://code.jquery.com/ui/1.14.0/themes/base/jquery-ui.css"> -->
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css"> -->
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css"> -->
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+</head>
+
+<body> 
+
+<!-- ------------------------------------------ section navbar-------------------------------------  -->
+
+    <header>
+        <div class="both">
+        
+        <div class="logo-clothes">
+            <a href="/home">
+                <img src="{{asset('images/logoClothes.png')}}" alt="logo_clothes">
+            </a>
+        </div>
+        <div class="hamburger" onclick="toggleMenu()">
+            <i class="fas fa-bars"></i>
+        </div>
+        </div>
+        <div class="nav-half">
+            <div class="button-nav">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-dark button-search " type="submit">Search</button>
+            </div>
+            <nav class="navigation">
+                <div class="nav-link">
+                    <!-- <a href="/services">Services</a>
+                    <a href="/contact">Contact</a> -->
+                </div>
+                <div class="nav-btn">
+                    <a class="login" href="{{ route('login') }}">Login </a>
+                    <a class="register" href="/register">Rigester</a>
+                    <a class="logout" href="{{ route('logout') }}"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    Logout
+                    </a>
+                </div>
+            </nav>
+        </div>
+
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+        </form>
+
+    </header>
+
+<!-- ------------------------------------------ end section navbar-------------------------------------  -->
+
+
+    <div class="container-new ">
+        @yield('content')
+    </div>
+
+    <!-- <footer class="footer">
+        <p class="footer-title mt-2">Copyrights @ <span>Lavender</span></p>
+        <div class="social-icons">
+            <a href="https://www.facebook.com" class="social-icons fab fa-facebook-f ico me-2 mt-2 " target="_blank">
+            </a>
+            <a href="https://www.instagram.com" class="social-icons fab fa-instagram ico me-2 mt-2 social" target="_blank">
+            </a>
+            <a href="https://www.twitter.com" class="social-icons fab fa-twitter ico me-2 mt-2 " target="_blank">
+            </a>
+            <a href="https://www.youtube.com" class="social-icons fab fa-youtube ico me-2 mt-2 social" target="_blank">
+            </a>
+        </div>
+    </footer> -->
+<!-- ------------------------------------------ section footer-------------------------------------  -->
+    <div class="footer">
+        <footer>
+            <div class="container-footer">
+                <div class="wrapper">
+                    <div class="footer-widget">
+                        <a href="">
+                            <img src="{{asset('images/logolavend.jpg')}}" class="logo">
+                        </a>
+                        <p class="desc">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        </p>
+                        <ul class="socials">
+                                <li><a href="https://www.facebook.com"> <i class="social-icons fab fa-facebook-f ico me-2 mt-2 " target="_blank"></i></a></li>
+                                <li><a href="https://www.twitter.com"> <i class="social-icons fab fa-twitter ico me-2 mt-2 " target="_blank"></i></a></li>
+                                <li><a href="https://www.instagram.com"> <i class="social-icons fab fa-instagram ico me-2 mt-2 social" target="_blank"></i></a></li>
+                                <li><a href="https://www.youtube.com"> <i class="social-icons fab fa-youtube ico me-2 mt-2 social" target="_blank"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-widget plus">
+                        <h6>Quick Link</h6>
+                        <ul class="links">
+                            <li><a href="">Home</a></li>
+                            <li><a href="">About</a></li>
+                            <li><a href="">Services</a></li>
+                            <li><a href="">Products</a></li>
+                            <li><a href="">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-widget plus">
+                        <h6>Services </h6>
+                        <ul class="links">
+                            <li><a href="">web design </a></li>
+                            <li><a href="">web development</a></li>
+                            <li><a href="">seo optimization</a></li>
+                            <li><a href="">blog writing </a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-widget plus">
+                        <h6>Help &amp; Support </h6>
+                        <ul class="links">
+                            <li><a href="">support center</a></li>
+                            <li><a href="">live chat</a></li>
+                            <li><a href="">FAQ</a></li>
+                            <li><a href="">terms &amp; coditions</a></li>  
+                        </ul>
+                    </div>
+                </div>
+                <div class="copyright-wrapper">
+                    <p>   &copy;2025 . All rights reseved. designed by Jessy / Zeina / Maya / Jawa .</p> 
+                </div>
+            </div> 
+        </footer>
+    </div>
+
+
+<!-- ------------------------------------------ end section footer-------------------------------------  -->
+
+
+<!-- ************ -->
+
+    <!-- <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script> -->
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script> -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script> -->
+    <!-- <script src="https://code.jquery.com/jquery-3.7.1.js"></script> -->
+    <!-- <script src="https://code.jquery.com/ui/1.14.0/jquery-ui.js"></script> -->
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script> -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script> -->
+    <!-- <script type="module"> 
+    import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+
+//   const swiper = new Swiper(...)
+</script>-->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="{{ asset('java/main.js') }}"></script>
+</body>
+</html>
